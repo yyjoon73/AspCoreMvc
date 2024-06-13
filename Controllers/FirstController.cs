edc84e77f21811ab124317642be599875213c07c
yyjoon7000 <yyjoon7000@nate.com>
2024-06-13 14:04:09 +0900

    초기 커밋.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreMvc.Controllers
+{
+    public class FirstController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public string Index()
+        //{
+        //    return "안녕하세요";
+        //}
+
+        public object Index()
+        {
+            return new { Name = "백아", Age = 78 };
+        }
+
+
+        public IActionResult WithoutView()
+        {
+            return View();
+        }
+    }
+}
